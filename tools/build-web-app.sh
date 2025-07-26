be825654bb3f542d5496aa775df9670c238083b1
Thomas Schaub <t.schaub@gmx.de>
2025-07-26 10:49:22 +0200

    Add build-web-app.sh

---
+++ tools/build-web-app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+html_dir=resources/html
+
+# Verify
+
+if [ ! -d $html_dir ]; then
+    echo "Error: $html_dir not found."
+    exit 1
+fi
+
+# Build
+
+build_dir=$(mktemp -d)
+
+emcmake cmake -S . -B $build_dir -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake --build $build_dir
+
+# Deploy
+
+app_dir=$(mktemp -d)
+
+cp $build_dir/{glapd-web.js,glapd-web.wasm} $app_dir
+cp $html_dir/* $app_dir
+
+echo "App is in $app_dir"
