0fa4ccb915a7d74f5961dea120ff3c63c5777e69
Thomas Schaub <t.schaub@gmx.de>
2026-01-16 17:04:24 +0100

    Add dev-httpd.py

--- readme.md
+++ readme.md
@@ -2,10 +2,20 @@
 
 ## Web
 
+CMake configure and initial build
+
 ```
 emcmake cmake -S . -B build/web -GNinja
 cmake --build build/web
 ```
+
+Run httpd (automatically rebuilds JS + WASM):
+
+```
+python toold/dev-httpd.py
+```
+
+Or do it manually
 
 ```
 cp resources/html/* build/web
---
+++ tools/dev-httpd.py
@@ -0,0 +1,61 @@
+import http.server
+import os.path
+import subprocess
+
+build_dir = 'build/web'
+html_dir = 'resources/html'
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_file(os.path.join(html_dir, 'index.html'))
+        elif self.path in ['/glapd-web.js', '/glapd-web.wasm']:
+            self.cmake_build()
+            path = os.path.join(build_dir, self.path[1:])
+            self.send_file(path)
+        else:
+            path = os.path.join(html_dir, self.path[1:])
+            self.send_file(path)
+
+    def cmake_build(self):
+        subprocess.run(['cmake', '--build', build_dir])
+
+    def send_file(self, path: str):
+        if not os.path.exists(path):
+            print(f'{path} does not exist, sending 404')
+            self.send_404()
+            return
+
+        # Send headers
+
+        if path.endswith('.html'):
+            content_type = 'text/html'
+        elif path.endswith('.js'):
+            content_type = 'text/javascript'
+        elif path.endswith('.wasm'):
+            content_type = 'application/wasm'
+        else:
+            raise ValueError('Unknown file extension')
+
+        self.send_response(200)
+        self.send_header('Content-Type', content_type)
+        self.end_headers()
+
+        # Send body
+
+        with open(path, 'rb') as f:
+            self.wfile.write(f.read())
+
+    def send_404(self):
+        self.send_error(404)
+
+def main():
+    server_address = ('', 8000)
+    server = http.server.HTTPServer(server_address, Handler)
+
+    print(f'Starting server at http://127.0.0.1:{server_address[1]}')
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
